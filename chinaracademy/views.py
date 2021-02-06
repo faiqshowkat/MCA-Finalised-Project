cada38bbf5d1c9cd1e719134492506a3644676f8
faiq <faiqshowkat@icloud.com>
2021-02-06 16:23:13 +0530

    login logout homepage workiing

--- chinaracademy/views.py
+++ chinaracademy/views.py
@@ -6,6 +6,6 @@ def frontpage(request):
     return render(request, 'frontpage.html')
 
 
-@login_required(login_url='accounts: signup')
+@login_required(login_url='frontpage')
 def homepage(request):
     return render(request, 'homepage.html')
